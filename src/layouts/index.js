3cc3d5dc79823137521e5cf6bb76d70a66ad9b6f
vision669 <32115565+vision669@users.noreply.github.com>
2018-09-09 20:14:21 -0500

    initial commit

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -10,7 +10,7 @@ const Layout = ({ children, data }) => (
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
-        { name: 'description', content: 'Sample' },
+        { name: 'description', content: 'let see if this is happening' },
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
